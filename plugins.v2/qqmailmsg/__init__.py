32b9cf6af189298e6264e956d34ac679b9f4bacf
zhangxuri198 <31956695+zhangxuri198@users.noreply.github.com>
2024-12-30 17:12:45 +0800

    Update __init__.py
    1.1

--- plugins.v2/qqmailmsg/__init__.py
+++ plugins.v2/qqmailmsg/__init__.py
@@ -70,7 +70,7 @@ class QQMailMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Wecom_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "zhangxuri198"
     # 作者主页
